210aa1de122fe2494768869faa6770b2d75f3e32
Robert Von Der Becke <vonderbeckememberships@gmail.com>
2022-05-19 21:21:33 -0500

    fixed sidebar

--- src/routes/dashboard.component.jsx
+++ src/routes/dashboard.component.jsx
@@ -36,40 +36,28 @@ const cardInfo = [
 const Dashboard = () => {
 	const { currentUser } = useContext(UserContext);
 	const nav = useNavigate();
+
 	return (
 		<div className='dashboard-container'>
 			<div className='sideBar'>
-				<div className='dots'>
-					<div class='one'>&#8226;</div>
-					<div class='two'>&#8226;</div>
-					<div class='three'>&#8226;</div>
-					<div class='four'>&#8226;</div>
-					<div class='five'>&#8226;</div>
-					<div class='six'>&#8226;</div>
+				<div className='nav-container'>
+					<input class='checkbox' type='checkbox' name='' id='' />
+					<div className='hamburger-lines'>
+						<span className='line line1'></span>
+						<span className='line line2'></span>
+						<span className='line line3'></span>
+					</div>
+					<div className='menu-items'>
+						<label htmlFor="cal" id='cal'>Calender</label>
+						<button className='cal'name='cal'/>
+						<label htmlFor="reg" id='reg'>Documents</label>
+						<button className='reg'name='reg'/>
+						<label htmlFor="event" id='event'>Events</label>
+						<button className='event'name='event'/>
+						<label htmlFor="profile" id='profile'>Profile</label>
+						<button className='profile'name='profile'/>
+					</div>
 				</div>
-				<span className='x'>&#x2715;</span>
-				<ul>
-					<li>
-						<img class='calendar' src='/images/calendar.png' alt='' />
-						<p>Calander</p>
-					</li>
-					<li>
-						<img class='reg' src='/images/registerOnline.png' alt='' />
-
-						<p>Online Registration</p>
-					</li>
-					<li>
-						<img class='event' src='/images/events.png' alt='' />
-
-						<p>Event Sign</p>
-					</li>
-					<li>
-						<NavLink to='/user/profile'>
-							<img class='user' src='/images/user.png' alt='' />
-							Account Profile
-						</NavLink>
-					</li>
-				</ul>
 			</div>
 			<div className='main-content'>
 				{cardInfo.map((item) => (
